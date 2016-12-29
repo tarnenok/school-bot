176f73a56d908dbbdb640c51172d51aae6bd3b54
Tarnenak Yauheni <tarnenok@gmail.com>
2016-12-29 22:26:07 +0300

    fix date again

--- src/TelegramBot.WebApi/Services/SchoolNewService.cs
+++ src/TelegramBot.WebApi/Services/SchoolNewService.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Net.Http;
 using System.Threading.Tasks;
+using AngleSharp.Extensions;
 using AngleSharp.Parser.Html;
+using Microsoft.Extensions.Logging;
 using RestSharp;
 using TelegramBot.WebApi.DB.Models;
+using TelegramBot.WebApi.DB.Services;
 using TelegramBot.WebApi.Extensions;
 using TelegramBot.WebApi.Models;
 
@@ -13,6 +16,13 @@ namespace TelegramBot.WebApi.Services
 {
     public class SchoolNewService
     {
+        private readonly ILogger _logger;
+
+        public SchoolNewService(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger(nameof(SchoolNewsService));
+        }
+
         private const string DATE_ATTR = "map";
 
         public async Task<IEnumerable<SchoolNews>> GetNewsAsync(DateTime dateTime)
@@ -39,7 +49,7 @@ namespace TelegramBot.WebApi.Services
                     ImageUrl = domain + element.ChildNodes[0].TextContent,
                     Title = element.ChildNodes[1].TextContent,
                     Url = domain + element.ChildNodes[2].TextContent,
-                    Date = DateTime.ParseExact("dd.MM.yyyy", element.ChildNodes[3].TextContent, CultureInfo.InvariantCulture)
+                    Date = DateTime.ParseExact(element.ChildNodes[3].TextContent, "dd.MM.yyyy", CultureInfo.InvariantCulture)
                 };
             }
         }
