3aecf36cfe4eb5a550b763fd99ffc64c84ec5895
Tarnenak Yauheni <tarnenok@gmail.com>
2017-01-07 20:50:02 +0300

    fix deplo.sh names

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ DOCKER_PASSWORD=$2
 publishFolder = 'bin/publish'
 imageName = 'tarnenok/school-bot'
 
-dotnet publish -o publishFolder -c Release
+dotnet publish -o $publishFolder -c Release
 
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker build publishFolder -t imageName
-docker push imageName
+docker build $publishFolder -t $imageName
+docker push $imageName
